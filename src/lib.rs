1d16f205188e6c59407870decb6daf623ae467af
Jona Stubbe (CrystalGamma) <dev@jona-stubbe.de>
2015-09-24 09:08:43 +0200

    Some font loading

--- src/lib.rs
+++ src/lib.rs
@@ -45,12 +45,12 @@ pub struct NonNativeConf;
 
 pub struct Run<'a>(pub &'a str, pub ScriptCode);
 
-pub fn make_default_streak(run: &Run, main_axis: MainAxis) {
+pub fn make_default_streak<'a>(run: &'a Run, main_axis: MainAxis) -> Streak<'a> {
 	use self::MainAxis::*;
 	Streak(run.0, run.1, StreakTypesetting(DirBehavior(Forward, Perpendicular), if run.1 == LATIN {
 		match main_axis {
-			Ltr | Ttb => Normal,
-			Rtl | Btt => Rotated
+			Ltr | Ttb => Rot180::Normal,
+			Rtl | Btt => Rot180::Rotated
 		}
 	} else {
 		unimplemented!();
@@ -69,22 +69,83 @@ impl<'a> Streak<'a> {
 
 pub fn bidi_algorithm(streaks: &mut [Streak]) {}
 
-pub struct FontPreference(Font);
+pub struct FontPreference<'a>(Font<'a>);
 
-impl FontPreference {
+/*impl FontPreference {
 	fn lookup(&self) -> Vec<Burst> {
 		Vec::new()
 	}
+}*/
+
+/// contains glyphs and typesetting information
+pub struct FontCollection<'a>(&'a[u8]);
+/// what features of the font collection do we want to use?
+pub struct FontConfiguration;
+
+/// contains character mapping
+pub struct Font<'a>(&'a[u8]);
+
+struct GlyphPoint(i16, i16);
+enum ContourSegment {
+	Line(GlyphPoint, GlyphPoint),
+	Curve(GlyphPoint, GlyphPoint, GlyphPoint)
 }
 
-pub struct Font;
+pub fn read_u32(data: &[u8]) -> Option<u32> {
+	if data.len() > 3 {
+		Some((data[0] as u32) << 24 | (data[1] as u32) << 16 | (data[2] as u32) << 8 | data[3] as u32)
+	} else {
+		None
+	}
+}
+
+pub fn read_u16(data: &[u8]) -> Option<u16> {
+	if data.len() > 1 {
+		Some((data[0] as u16) << 8 | data[1] as u16)
+	} else {
+		None
+	}
+}
+
+fn fourcc(tag: u32) -> String {
+	let mut s = String::with_capacity(4);
+	s.push((tag >> 24) as u8 as char);
+	s.push((tag >> 16) as u8 as char);
+	s.push((tag >> 8) as u8 as char);
+	s.push(tag as u8 as char);
+	s
+}
+
+macro_rules! try_opt {
+	($e:expr) => (match $e {Some(x)=>x, None=>return None})
+}
+
+fn scan_table_records(data: &[u8], start: usize, label: u32, num_tables: usize) -> Option<(usize, &[u8])> {
+	for pos in start..num_tables {
+		if try_opt!(read_u32(&data[12+16*pos..])) == label {
+			let start = read_u32(&data[12+16*pos+8..]).unwrap() as usize;
+			return Some((pos, &data[start..read_u32(&data[12+16*pos+12..]).unwrap() as usize+start]));
+		}
+	}
+	None
+}
+
+fn load_font_cmap(data: &[u8]) -> Option<Font> {
+	Some(Font(data))
+}
+
+pub fn load_font_collection<'a>(data: &'a[u8], fonts: Vec<&str>) -> Option<(FontCollection<'a>, Vec<Font<'a>>)> {
+	let num_tables = try_opt!(read_u16(&data[4..])) as usize;
+	let (_pos, cmap) = try_opt!(scan_table_records(data, 0, 0x636d6170, num_tables));
+	Some((FontCollection(data), vec![Font(cmap)]))
+}
 
 struct GlyphIndex(u16);
 
-struct Burst<'a>(Vec<GlyphIndex>, &'a Font, StreakTypesetting);
+struct Burst<'a, 'b: 'a>(Vec<GlyphIndex>, &'a FontCollection<'b>, StreakTypesetting);
 
-pub struct TypeSetting<'a>(Vec<Burst<'a>>);
+pub struct TypeSetting<'a, 'b: 'a>(Vec<Burst<'a, 'b>>);
 
-fn typeset<'a>(streak: Streak, fonts: &'a FontPreference) -> TypeSetting<'a> {
+fn typeset<'a, 'b>(streak: Streak, fonts: &FontPreference<'a>) -> TypeSetting<'b, 'a> {
 	TypeSetting(Vec::new())
 }
